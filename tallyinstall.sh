a287be59faa9665af17e2f3e2cc8b81b38fc8371
honey-pablo <38726957+honey-pablo@users.noreply.github.com>
2024-09-11 14:31:16 +0100

    Update tallyinstall.sh

--- tallyinstall.sh
+++ tallyinstall.sh
@@ -8,6 +8,10 @@ sudo pip3 install argparse
 wget  https://github.com/josephdadams/TallyArbiter/raw/master/listener_clients/gpo-listener/gpo-listener.py
 wget https://raw.githubusercontent.com/honey-pablo/tally/main/config_gpo.json
 wget https://raw.githubusercontent.com/honey-pablo/tally/main/customiseconfig.sh
+wget https://raw.githubusercontent.com/honey-pablo/tally/main/launcher.sh
+https://raw.githubusercontent.com/honey-pablo/tally/main/tempcron
+chmod 755 launcher.sh
+crontab tempcron
 chmod +x customiseconfig.sh
 bash +x customiseconfig.sh
 sudo python3 gpo-listener.py
